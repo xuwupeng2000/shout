1b4559ecca775e3b793fa10cc631fe156146bf6a
Aaron <aaronhigh.nz@gmail.com>
2018-06-16 00:08:57 +1200

    fix typo

--- app/components/MainBottomNavigation.js
+++ app/components/MainBottomNavigation.js
@@ -57,7 +57,7 @@ class MainBottomNavigation extends PureComponent {
             onPress={() => this.props.navigateToPage(false, BottomNavigator.TABS.Alarm)}/>
 
           <ButtonNavigationActionNoColor
-            key="Services"
+            key="Service"
             icon={
               <TouchableOpacity>
                 <Image
@@ -66,7 +66,7 @@ class MainBottomNavigation extends PureComponent {
                   source={require('../../res/img/ic_services.png')}/>
               </TouchableOpacity>
             }
-            label={BottomNavigator.TABS.Alarm}
+            label={BottomNavigator.TABS.Service}
             onPress={() => this.props.navigateToPage(false, BottomNavigator.TABS.Service)}/>
       </BottomNavigation>
     );
